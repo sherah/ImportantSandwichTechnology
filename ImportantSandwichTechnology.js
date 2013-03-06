78383837d5bfc7938163ce06df68cf8211d3433f
Sherah <sherah@sherah.org>
2013-03-06 13:38:03 -0800

    added submit button/radio buttons and pared down to basic functionality. also styling happened.

--- ImportantSandwichTechnology.js
+++ ImportantSandwichTechnology.js
@@ -1,6 +1,27 @@
 if (Meteor.isClient) {
+  var sandwichOrder = {};
+
   Template.main.events({
     //where main template events will go.
+  });
+
+  Template.sandwichDenomination.events({
+    'click .choice' :  function(event){
+      console.log(event);
+    }
+  });
+
+  Template.submit.events({
+    'click .submit' : function(event){
+      event.preventDefault();
+      _.each($("input:radio[name=sandoChoice]"), function(val){
+        if($(val).is(':checked')){
+        console.log(val.value);
+      }
+
+      });
+      
+    }
   });
 }
 
